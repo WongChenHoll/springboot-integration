0193e461d23a14fb0761c5a7fccdfcb3d17ea8ae
chenghaojason <whx20100101@163.com>
2022-03-29 17:13:33 +0800

    添加mybatis工程

---
+++ springboot-mybatis/src/main/java/com.jason.mybatis/MybatisApplication.java
@@ -0,0 +1,15 @@
+package com.jason.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WongChenHoll
+ * @date 2022-3-29 17:02
+ **/
+@SpringBootApplication
+public class MybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
